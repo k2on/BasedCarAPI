e985235c16cf946a6ec2167ea7f130dbfa96f055
Max Koon <k2@Maxs-MacBook-Pro.local>
2023-11-23 21:36:09 -0600

    2024-2020

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use std::{path::Path, fs::{File, create_dir_all}, io::{BufReader, BufWriter}};
+use std::{path::Path, fs::{File, create_dir_all}, io::{BufReader, BufWriter}, process::{exit, Command}};
 
+use scraper::{Html, Selector};
 use serde::{Serialize, Deserialize};
+use tokio::fs::{File as TFile, remove_file};
+use tokio::io::AsyncWriteExt;
+use futures::stream::StreamExt;
+
 
 const BASE_URL: &str = "https://www.evoxstock.com/ajaxpickyourvehicle.asp";
 
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Years {
-    years: Vec<String>,
+struct Car {
+    colors: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+enum Color {
+    BLACK,
+    WHITE,
+    SILVER,
+    BLUE,
+    YELLOW,
+    GREEN,
+    RED,
+    BAIGE,
+    ORANGE
 }
 
 #[tokio::main]
 async fn main() {
     let years = get_years(false).await;
+
     for year in years {
         println!("Year: {year}");
         let makes = get_makes(&year, false).await;
@@ -20,15 +39,68 @@ async fn main() {
         for make in makes {
             println!("Make: {make}");
             let models = get_models(&year, &make, false).await;
-            println!("Models: {models:?}");
-            // for model in models {
-            //     println!("Model: {model}");
-            // }
+            for model in models {
+                println!("Model: {model}");
 
+                let colors = get_colors(&year, &make, &model, false).await;
+                println!("Colors: {colors:?}");
+            }
         }
     }
 }
 
+impl Color {
+    fn code(&self) -> i32 {
+        match self {
+            Color::BLACK => 1,
+            Color::WHITE => 2,
+            Color::SILVER => 4,
+            Color::BLUE => 8,
+            Color::YELLOW => 16,
+            Color::GREEN => 32,
+            Color::RED => 64,
+            Color::BAIGE => 128,
+            Color::ORANGE => 256,
+        }
+    }
+
+    fn str(&self) -> String {
+        match self {
+            Color::BLACK => String::from("Black"),
+            Color::WHITE => String::from("White"),
+            Color::SILVER => String::from("Silver"),
+            Color::BLUE => String::from("Blue"),
+            Color::YELLOW => String::from("Yellow"),
+            Color::GREEN => String::from("Green"),
+            Color::RED => String::from("Red"),
+            Color::BAIGE => String::from("Baige"),
+            Color::ORANGE => String::from("Orange"),
+        }
+    }
+}
+
+async fn get_color(year: &str, make: &str, model: &str, color: &Color) -> Option<String> {
+    let color_code = color.code();
+    let url = format!("https://www.evoxstock.com/results.asp?search=1&itemtype=1&color={color_code}&shot=0&library=0&txtkeys1={year} {make} {model}");
+    let resp = reqwest::get(url).await.unwrap();
+    let html = resp.text().await.unwrap();
+    let frag = Html::parse_fragment(&html);
+    let selector = Selector::parse("#ImageGrid").unwrap();
+    let img = frag.select(&selector).into_iter().nth(0);
+    if let Some(el) = img {
+        let sel = Selector::parse("img").unwrap();
+        let mut i = el.select(&sel);
+
+        let src = i.next().unwrap().value().attr("src");
+        if let Some(src) = src {
+            Some(format!("https://www.evoxstock.com{src}"))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
 
 async fn get_years(force: bool) -> Vec<String> {
     get_data("data/years.json", "", "CarYears,", force).await
@@ -41,6 +113,87 @@ async fn get_makes(year: &str, force: bool) -> Vec<String> {
 async fn get_models(year: &str, make: &str, force: bool) -> Vec<String> {
     get_data(&format!("data/years/{year}/{make}/models.json"), &format!("?caryear={year}&carmake={make}"), "CarModels,", force).await
 }
+
+
+async fn get_colors(year: &str, make: &str, model: &str, force: bool) -> Vec<String> {
+    let colors_all: Vec<Color> = vec![
+        Color::BLACK,
+        Color::WHITE,
+        Color::SILVER,
+        Color::BLUE,
+        Color::YELLOW,
+        Color::GREEN,
+        Color::RED,
+        // Color::BAIGE,
+        // Color::ORANGE,
+    ];
+
+    let path_str = format!("data/years/{year}/{make}/{model}/colors.json");
+    let path = Path::new(&path_str);
+    if path.exists() && !force {
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    } else {
+        let mut colors = Vec::new();
+
+        for color in colors_all {
+            println!("Getting {color:?}");
+            if let Some(url) = get_color(&year, &make, &model, &color).await {
+                let color_str = color.str();
+                colors.push(color_str.clone());
+
+                let path_str = format!("data/years/{year}/{make}/{model}/colors/{color_str}_base.jpeg");
+                let path_out = format!("data/years/{year}/{make}/{model}/colors/{color_str}.png");
+                let path = Path::new(&path_str);
+
+                if let Some(parent) = path.parent() {
+                    create_dir_all(parent).unwrap();
+                }
+
+                let mut file = TFile::create(path).await.unwrap();
+
+
+                let resp = reqwest::get(url).await.unwrap();
+                let mut stream = resp.bytes_stream();
+
+                while let Some(item) = stream.next().await {
+                    let chunk = item.expect("Failed to read chunk");
+                    file.write_all(&chunk).await.expect("Failed to write to file");
+                }
+
+                println!("Removing bg...");
+
+                let mut command = Command::new("rembg");
+                command.arg("i").arg(path_str.clone()).arg(path_out);
+
+                let output = command.output().expect("Failed to execute command");
+                if output.status.success() {
+                    println!("Background removed! Deleting original file...");
+
+                    remove_file(path_str).await.unwrap();
+
+                    println!("File deleted!");
+                } else {
+                    let error_message = String::from_utf8_lossy(&output.stderr);
+                    eprintln!("Error: {}", error_message);
+                }
+
+            }
+        }
+        
+        if let Some(parent) = path.parent() {
+            create_dir_all(parent).unwrap();
+        }
+
+        let file = File::create(path).unwrap();
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &colors).unwrap();
+
+        colors
+    }
+}
+
 
 async fn get_data(path: &str, args: &str, first: &str, force: bool) -> Vec<String> {
     let path = Path::new(path);
